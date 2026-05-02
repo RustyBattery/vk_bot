c99758bd65c7430b6b84b81cf83f5701ce722087
Adukova Margarita <adukova_m@100points.ru>
2026-05-02 19:47:34 +0700

    add module tables and seeder

---
+++ app/Models/ModuleQuestion.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+#[Fillable(['module_id', 'question', 'explanation'])]
+class ModuleQuestion extends Model
+{
+    public function module(): BelongsTo
+    {
+        return $this->belongsTo(Module::class);
+    }
+
+    public function answers(): HasMany
+    {
+        return $this->hasMany(ModuleAnswer::class);
+    }
+}
