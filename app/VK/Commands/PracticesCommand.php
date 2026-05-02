def1a38645cc99397abc04ca0434b2b4327fe1f6
Adukova Margarita <adukova_m@100points.ru>
2026-05-03 00:29:39 +0700

    update practices command

--- app/VK/Commands/PracticesCommand.php
+++ app/VK/Commands/PracticesCommand.php
@@ -2,6 +2,8 @@
 
 namespace App\VK\Commands;
 
+use App\VK\DTO\ButtonDTO;
+use App\VK\DTO\KeyboardDTO;
 use Illuminate\Http\Client\ConnectionException;
 
 class PracticesCommand extends Command
@@ -14,8 +16,29 @@ class PracticesCommand extends Command
      */
     public function handle(int $user_id, ?object $payload = null): void
     {
-        $message = 'Раздел "Журнал практики" в разработке';
+        $message = "Статистика:\n\n";
+        $message .= "Всего сессий:". null . "\n";
+        $message .= "Средн. снижение стресса:". null . "\n";
+        $message .= "Средн. уровень до:". null . "\n";
+        $message .= "Средн. уровень после:". null . "\n";
 
-        $this->messageService->send($user_id, $message);
+        $message .= "\n\n";
+
+        $buttons = [
+            [new ButtonDTO(
+                label: 'Записать новую сессию',
+                payload: json_encode(['command' => 'practice_add']),
+            )],
+            [new ButtonDTO(
+                label: 'История практики',
+                payload: json_encode(['command' => 'practice_history']),
+            )],
+            [new ButtonDTO(
+                label: 'Меню',
+                payload: json_encode(['command' => 'menu']),
+            )],
+        ];
+
+        $this->messageService->send($user_id, $message, new KeyboardDTO($buttons, false, false));
     }
 }
